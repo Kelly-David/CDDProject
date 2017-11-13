f7d4444ff837f3527aaaaf7593bb7d14af2c7776
kelly-david <davidkelly.tlf@gmail.com>
2017-11-13 09:42:37 +0000

    Creature cpp

---
+++ part1/Creature.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: david.kelly 
+ * @Date: 2017-11-13 09:00:39 
+ * @Last Modified by: david.kelly
+ * @Last Modified time: 2017-11-13 09:16:06
+ */
+
+/*! @file Creature.cpp
+    @brief Implements Creature class
+*/
+
+#include "Creature.h"
+#include <stdlib.h>  
+#include <iostream>
+
+void Creature::grow() {
+    ++age;
+}
+
+void Creature::swim(int xC, int yC) {
+    x = xC;
+    y = yC;
+}
+
+void Creature::eat() {
+    age = 0;
+}
+
+int Creature::getX() {
+    return x;
+}
+
+int Creature::getY() {
+    return y;
+}
+
+Creature::Creature(int a=0, int xC, int yC) 
+: age(a), x(xC), y(yC) { };
+
+
